e3f26a463df349ccb113e2bdd0e095b733dcf5ae
Filip Dubnička <dubnickaf@gmail.com>
2025-09-29 00:43:48 +0200

    autoscaling removed

--- main.tf
+++ main.tf
@@ -33,20 +33,16 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
-
-  name = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-
-  image_id            = data.aws_ami.app_ami.id
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id = module.blog_vpc.public_subnets[0]
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
 
 module "blog_alb" {
@@ -67,6 +63,12 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   ]
 
